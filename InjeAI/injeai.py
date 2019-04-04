4e6fd0d26654cf3296e339d98ceb35c4fe087b58
pasteldew <PastelDew@gmail.com>
2019-04-04 21:27:35 +0900

    Epoch 50 -> 100

--- InjeAI/injeai.py
+++ InjeAI/injeai.py
@@ -69,7 +69,7 @@ class InjeAIConfig(Config):
     NUM_CLASSES = 1 + 1  # Background + InjeAI
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 50
+    STEPS_PER_EPOCH = 100
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
