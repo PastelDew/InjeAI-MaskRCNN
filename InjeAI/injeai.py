Bug fix : Masking

InjeAI/injeai.py
@@ -214,7 +214,7 @@ class InjeAIDataset(utils.Dataset):
             elif mask_image.shape[-1] == 3:
                 mask_image = skimage.color.rgb2gray(mask_image)
             mask_image = mask_image.astype(np.uint8)
-            mask_image = (mask_image > 0) == 0
+            mask_image = mask_image > 0
             mask[:, :, i] = mask_image
             class_ids[i] = class_id
 